3fd10251a2f5aa8ab5438bd3f37afdcaf969b042
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2012-08-16 13:15:59 +0000

    init

---
+++ trunk/LedQbEmu/LedQbEmu/QBWidget.h
@@ -0,0 +1,12 @@
+#ifndef QBWidget_h__
+#define QBWidget_h__
+
+#include <QGLWidget>
+
+class QBWidget : public QGLWidget
+{
+public:
+	QBWidget(QWidget* parent = NULL);
+	~QBWidget(void);
+};
+#endif // QBWidget_h__
---
+++ trunk/LedQbEmu/LedQbEmu/ledqbemu.h
@@ -0,0 +1,23 @@
+#ifndef LEDQBEMU_H
+#define LEDQBEMU_H
+
+#include <QtGui/QMainWindow>
+#include "ui_ledqbemu.h"
+
+class QBWidget;
+
+class LedQbEmu : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	LedQbEmu(QWidget *parent = 0, Qt::WFlags flags = 0);
+	~LedQbEmu();
+
+private:
+	Ui::LedQbEmuClass ui;
+
+	QBWidget* m_qbWidget;
+};
+
+#endif // LEDQBEMU_H
