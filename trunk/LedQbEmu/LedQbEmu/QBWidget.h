eab9580c07a16bb34caf1c415f36e56f80463d2d
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2012-08-16 14:06:21 +0000

    rotating sphere

--- trunk/LedQbEmu/LedQbEmu/QBWidget.h
+++ trunk/LedQbEmu/LedQbEmu/QBWidget.h
@@ -8,5 +8,29 @@ class QBWidget : public QGLWidget
 public:
 	QBWidget(QWidget* parent = NULL);
 	~QBWidget(void);
+
+protected:
+	void initializeGL();
+	void resizeGL(int width, int height);
+	void paintGL();
+
+private:
+	void mousePressEvent(QMouseEvent *event);
+	void mouseMoveEvent(QMouseEvent *event);
+	void setXRotation(int angle);
+	void setYRotation(int angle);
+	void setZRotation(int angle);
+
+	void gltDrawSphere(GLfloat fRadius, GLint iSlices, GLint iStacks);
+
+	int m_xRot;
+	int m_yRot;
+	int m_zRot;
+	QPoint m_lastPos;
+
+	QColor m_colorBackground;
+	GLfloat m_onColor[4];
+	GLfloat m_offColor[4];
 };
+
 #endif // QBWidget_h__
