bea8d23c0c1ce0ff5759fdc00162bfc739242100
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2012-10-23 20:18:53 +0000

    функции прямого доступа к памяти LedCubeData

--- LedCube/libs/LedCubeBasic/LedCubeData.cpp
+++ LedCube/libs/LedCubeBasic/LedCubeData.cpp
@@ -1,36 +1,50 @@
 #include "LedCubeData.h"
- 
+
+//установка значений
 void LedCubeData::setLed(byte x, byte y, byte z, bool on)
 {
-   bitWrite(m_data[z][x], y, on);
+	bitWrite(m_data[z][x], y, on);
 }
  
 void LedCubeData::setLevel(byte z, byte data[8])
 {
-  memcpy(m_data[z], data, 8*sizeof(byte));
+	memcpy(m_data[z], data, 8*sizeof(byte));
 }
  
 void LedCubeData::setData(byte data[8][8])
 {
-  memcpy(m_data, data, 8*8*sizeof(byte));
-}
- 
-bool LedCubeData::isLedSet(byte x, byte y, byte z)
-{
-  return bitRead(m_data[z][x], y);
-}
- 
-byte* LedCubeData::level(byte z)
-{
-  return m_data[z];
-}
- 
-byte LedCubeData::line(byte x, byte z)
-{
-  return m_data[z][x];
+	memcpy(m_data, data, 8*8*sizeof(byte));
 }
 
 void LedCubeData::clear(byte value)
 {
 	memset(m_data, 0, 8*8*sizeof(byte));
 }
+
+//получение значений
+bool LedCubeData::isLedSet(byte x, byte y, byte z) const
+{
+	return bitRead(m_data[z][x], y);
+}
+
+byte LedCubeData::line(byte x, byte z) const
+{
+	return m_data[z][x];
+}
+
+//жёсткий доступ
+byte* LedCubeData::level(byte z)
+{
+	return m_data[z];
+}
+
+byte* LedCubeData::operator[](byte z)
+{
+	return level(z);
+}
+
+byte& LedCubeData::line(byte x, byte z)
+{
+	return  m_data[z][x];
+}
+
--- LedCube/libs/LedCubeBasic/LedCubeData.h
+++ LedCube/libs/LedCubeBasic/LedCubeData.h
@@ -10,19 +10,25 @@
 class LedCubeData
 {
 public:
+	//установка значений
 	void setLed(byte x, byte y, byte z, bool on);	//изменить состояние диода
 	void setLevel(byte z, byte data[8]);			//изменить уровень
 	void setData(byte data[8][8]);					//изменить весь куб
-
-	bool isLedSet(byte x, byte y, byte z);			//получить значение диода
-	byte* level(byte z);							//получить указатель на уровень
-	byte line(byte y, byte z);						//получить значение линии
-
+	
 	void clear(byte value = 0);						//заполнить все линии заданным значением
+
+	//мягкий доступ
+	bool isLedSet(byte x, byte y, byte z) const;	//получить значение диода
+	byte line(byte x, byte z) const;				//получить значение линии
+	
+	//жёсткий доступ
+	byte* level(byte z);							//получить указатель на уровень
+	byte* operator[](byte z);						//получить указатель на уровень
+	byte& line(byte x, byte z);						//получить значение линии
  
 private:
-	friend class LedCube;//для прямого доступа к m_data
-	byte m_data[8][8]; // (z; x)
+	friend class LedCube;	// для прямого доступа к m_data
+	byte m_data[8][8];		// (z; x)
 };
 
 #endif LED_CUBE_DATA_H
