50d90aa8df50d055a6157ea72cb0f096ead15fd6
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2012-10-23 19:32:58 +0000

    комментарии в LedCube

--- trunk/LedCube/libs/LedCubeBasic/LedCube.cpp
+++ trunk/LedCube/libs/LedCubeBasic/LedCube.cpp
@@ -1,43 +1,45 @@
 #include "LedCube.h"
 
 LedCube::LedCube(int latchPin, int clockPin, int dataPin)
-: m_latchPin(latchPin), m_clockPin(clockPin), m_dataPin(dataPin)
+	: m_latchPin(latchPin), m_clockPin(clockPin), m_dataPin(dataPin)
 {
-  pinMode(m_latchPin, OUTPUT);
-  pinMode(m_clockPin, OUTPUT);
-  pinMode(m_dataPin, OUTPUT);
+	pinMode(m_latchPin, OUTPUT);	//все пины посылают сигналы
+	pinMode(m_clockPin, OUTPUT);
+	pinMode(m_dataPin, OUTPUT);
 }
  
 void LedCube::lightOne(byte x, byte y, byte z)
 {
-  int curX = 7;
-  digitalWrite(m_latchPin, LOW);
-   
-  shiftOut(m_dataPin, m_clockPin, MSBFIRST, 1 << z);//layer = z
-  for(; curX > x; --curX) shiftOut(m_dataPin, m_clockPin, MSBFIRST, 0);// lines after x
-  shiftOut(m_dataPin, m_clockPin, MSBFIRST, 1 << y);// line x, led y
-  --curX;
-  for(; curX >= 0; --curX) shiftOut(m_dataPin, m_clockPin, MSBFIRST, 0);// lines before x
- 
-  digitalWrite(m_latchPin, HIGH);
+	int curX = 7;//текущая линия
+	digitalWrite(m_latchPin, LOW);	//теперь изменения в регистрах не отразятся на кубе
+
+	//регистры наполняются с последнего, сначала регистр уровня, потом 7-я линия, 6-я и так далее
+	shiftOut(m_dataPin, m_clockPin, MSBFIRST, 1 << z);//задействуем уровень z
+	for(; curX > x; --curX) shiftOut(m_dataPin, m_clockPin, MSBFIRST, 0);//пропускаем все линии с 7ой до x-овой
+	shiftOut(m_dataPin, m_clockPin, MSBFIRST, 1 << y);//зажигаем на x-овой линии диод y
+	--curX;
+	for(; curX >= 0; --curX) shiftOut(m_dataPin, m_clockPin, MSBFIRST, 0);//пропускаем все оставшиеся линии до нолевой
+
+	digitalWrite(m_latchPin, HIGH);	//теперь изменения вступят в силу
 }
  
 void LedCube::lightLevel(byte z, byte data[8])
 {
-  digitalWrite(m_latchPin, LOW);
+  digitalWrite(m_latchPin, LOW);	//теперь изменения в регистрах не отразятся на кубе
    
-  shiftOut(m_dataPin, m_clockPin, MSBFIRST, 1 << z);//layer = z
-  for(int i = 7; i >= 0; --i) shiftOut(m_dataPin, m_clockPin, MSBFIRST, data[i]);
+  shiftOut(m_dataPin, m_clockPin, MSBFIRST, 1 << z);//задействуем уровень z
+  for(int i = 7; i >= 0; --i) shiftOut(m_dataPin, m_clockPin, MSBFIRST, data[i]);//линии передаются начиная с 7-ой
  
-  digitalWrite(m_latchPin, HIGH);
+  digitalWrite(m_latchPin, HIGH);	//теперь изменения вступят в силу
 }
  
 void LedCube::lightCube(byte data[8][8])
 {
-  for(int i = 0; i <= 7; ++i) lightLevel(i, data[i]);
+	//зажгём поочерёдно все уровни начиная с 0-ого. В каждый отдельный момент может гореть только один!
+	for(int i = 0; i <= 7; ++i) lightLevel(i, data[i]);
 }
  
 void LedCube::lightCube(LedCubeData& data)
 {
-  lightCube(data.m_data);
+  lightCube(data.m_data);//отобразим массив из LedCubeData
 }
--- trunk/LedCube/libs/LedCubeBasic/LedCube.h
+++ trunk/LedCube/libs/LedCubeBasic/LedCube.h
@@ -4,17 +4,23 @@
 #include <Arduino.h> 
 #include "LedCubeData.h"
 
+/*
+ *	Диодный куб 8x8x8.
+ *	Все размерности нумеруются с ноля. Этажи нумеруются по z, линии по x, диоды в линии по y
+ *	Любая функция зажигает переданные диоды и гасит все остальные.
+ *	Диоды в линии нумеруются с младщего бита, то есть 1 << 2 - второй диод (y = 2)
+ */
 class LedCube
 {
 public:
-  LedCube(int latchPin, int clockPin, int dataPin);
-  void lightOne(byte x, byte y, byte z);
-  void lightLevel(byte z, byte data[8]);
-  void lightCube(byte data[8][8]);
-  void lightCube(LedCubeData& data);
+  LedCube(byte latchPin, byte clockPin, byte dataPin);	//Все пины - цифроыве, PWM не используется
+  void lightOne(byte x, byte y, byte z);	//зажечь один диод
+  void lightLevel(byte z, byte data[8]);	//зажечь уровень на основе массива линий
+  void lightCube(byte data[8][8]);			//зажечь куб на основе массива уровней
+  void lightCube(LedCubeData& data);		//зажечь куб на основе LedCubeData
  
 private:
-  int m_latchPin, m_clockPin, m_dataPin;
+  byte m_latchPin, m_clockPin, m_dataPin;
 };
 
 #endif LED_CUBE_H
