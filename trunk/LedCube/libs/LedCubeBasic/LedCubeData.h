0daf03fb1a968b8be0d2637a727603cb5e60b8b5
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2012-10-23 19:49:27 +0000

    комментарии в LedCubeData

--- trunk/LedCube/libs/LedCubeBasic/LedCubeData.h
+++ trunk/LedCube/libs/LedCubeBasic/LedCubeData.h
@@ -3,21 +3,26 @@
 
 #include <Arduino.h> 
 
+/*
+ *	Полная информация о диодах в кубе 8x8x8.
+ *	Значения не инициализируются в конструкторе!
+ */
 class LedCubeData
 {
 public:
-  LedCubeData();
-  void setLed(byte x, byte y, byte z, bool on);
-  void setLevel(byte z, byte data[8]);
-  void setData(byte data[8][8]);
- 
-  bool isLedSet(byte x, byte y, byte z);
-  byte* level(byte z);
-  byte line(byte y, byte z);
+	void setLed(byte x, byte y, byte z, bool on);	//изменить состояние диода
+	void setLevel(byte z, byte data[8]);			//изменить уровень
+	void setData(byte data[8][8]);					//изменить весь куб
+
+	bool isLedSet(byte x, byte y, byte z);			//получить значение диода
+	byte* level(byte z);							//получить указатель на уровень
+	byte line(byte y, byte z);						//получить значение линии
+
+	void clear(byte value = 0);						//заполнить все линии заданным значением
  
 private:
-  friend class LedCube;
-  byte m_data[8][8]; // (z; y)
+	friend class LedCube;//для прямого доступа к m_data
+	byte m_data[8][8]; // (z; x)
 };
 
 #endif LED_CUBE_DATA_H
