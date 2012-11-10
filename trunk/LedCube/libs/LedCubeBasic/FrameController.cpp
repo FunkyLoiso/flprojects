2f4b172deb5983fba5a9e0598aff08f4d8e800bb
corealugly <corealugly@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2012-11-10 10:51:56 +0000

    frame controller class for framerate control

---
+++ trunk/LedCube/libs/LedCubeBasic/FrameController.cpp
@@ -0,0 +1,58 @@
+#include "FrameController.h"
+
+FrameController::FrameController()
+: m_frameEnd_mcs(micros())
+{
+	setCube(NULL);
+	setData(NULL);
+	setFrameInterval_mcs(0);
+}
+
+FrameController::FrameController(LedCube* cube, LedCubeData* data)
+: m_frameEnd_mcs(micros())
+{
+	setCube(cube);
+	setData(data);
+	setFrameInterval_mcs(0);
+}
+	
+void FrameController::setCube(LedCube* cube)
+{
+	m_cube = cube;
+}
+void FrameController::setData(LedCubeData* data)
+{
+	m_data = data;
+}
+	
+void FrameController::setFrameInterval_mcs(unsigned long time)
+{
+	m_frameInterval_mcs = time;
+}
+unsigned long FrameController::getFrameInterval_mcs() const
+{
+	return m_frameInterval_mcs;
+}
+
+void FrameController::setFPS(unsigned int fps)
+{
+	m_frameInterval_mcs = 1000000 / fps;
+}
+
+unsigned int FrameController::getFPS() const
+{
+	return 1000000 / m_frameInterval_mcs;
+}
+	
+void FrameController::begin()
+{
+	m_cube->turnOut();
+}
+void FrameController::end()
+{
+	while(micros() < m_frameEnd_mcs)
+	{
+		m_cube->lightCube(*m_data);
+	}
+	m_frameEnd_mcs += m_frameInterval_mcs;
+}
