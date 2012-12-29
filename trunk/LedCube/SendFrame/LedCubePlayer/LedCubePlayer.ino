69756a55bcbe139d69d87602024087578dcdf1e0
corealugly <corealugly@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2012-12-29 18:21:30 +0000

    this works fine

--- trunk/LedCube/SendFrame/LedCubePlayer/LedCubePlayer.ino
+++ trunk/LedCube/SendFrame/LedCubePlayer/LedCubePlayer.ino
@@ -12,48 +12,50 @@ template<int SIZE>
 class FrameQueue
 {
 public:
-	FrameQueue()
-	{
-		m_enqPtr = m_arr;
-		m_deqPtr = m_arr;
-		m_size = 0;
-	}
+  FrameQueue()
+  {
+    m_enqPtr = m_arr;
+    m_deqPtr = m_arr;
+    m_size = 0;
+  }
 
-	bool enqueue(byte data[8][8])
-	{
-		if(m_size == SIZE) return false;
+  bool enqueue(byte data[8][8])
+  {
+    if(m_size == SIZE) return false;
 
-		m_enqPtr->setData(data);
-		incrementPtr(m_enqPtr);
-		++m_size;
+    m_enqPtr->setData(data);
+    incrementPtr(m_enqPtr);
+    ++m_size;
 
-		return true;
-	}
+    return true;
+  }
 
-	bool dequeue(LedCubeData& data)
-	{
-		if(m_size == 0) return false;
+  bool dequeue(LedCubeData& data)
+  {
+    if(m_size == 0) return false;
 
-		data = *m_deqPtr;
-		incrementPtr(m_deqPtr);
-		--m_size;
+    data = *m_deqPtr;
+    incrementPtr(m_deqPtr);
+    --m_size;
 
-		return true;
-	}
+    return true;
+  }
 
-	byte getSize(){return m_size;}
+  byte getSize(){
+    return m_size;
+  }
 
 private:
-	LedCubeData m_arr[SIZE];
-	byte m_size;
-	LedCubeData* m_enqPtr;
-	LedCubeData* m_deqPtr;
+  LedCubeData m_arr[SIZE];
+  byte m_size;
+  LedCubeData* m_enqPtr;
+  LedCubeData* m_deqPtr;
 
-	void incrementPtr(LedCubeData*& ptr)
-	{
-		if(ptr != m_arr+SIZE-1) ++ptr;
-		else ptr = m_arr;
-	}
+  void incrementPtr(LedCubeData*& ptr)
+  {
+    if(ptr != m_arr+SIZE-1) ++ptr;
+    else ptr = m_arr;
+  }
 
 };
 
@@ -61,65 +63,73 @@ FrameQueue<10> queue;
 
 void setup()
 {
-	Serial.begin(115200);
+  Serial.begin(115200);
+  Serial.setTimeout(0);
 }
 
 void loop()
 {
-	static const unsigned long m_frameInterval_mcs = 1000000/*16667*/;
-	static unsigned long m_frameEnd_mcs = micros();
+  static const unsigned long m_frameInterval_mcs = 16667/*16667*/;
+  static unsigned long m_frameEnd_mcs = micros();
 
-	while(micros() < m_frameEnd_mcs)
-	{
-		receiveData();
-		cube.lightCube(data);
-	}
-	updateCurentFrame();
-	m_frameEnd_mcs += m_frameInterval_mcs;//рассчитаем время окончания следующего кадра
-	Serial.print(". ");
-	Serial.print(queue.getSize());
-	Serial.println(" in queue");
+  while(micros() < m_frameEnd_mcs)
+  {
+    receiveData();
+    cube.lightCube(data);
+  }
+  updateCurentFrame();
+  m_frameEnd_mcs += m_frameInterval_mcs;//рассчитаем время окончания следующего кадра
+  //	Serial.print(". ");
+  //	Serial.print(queue.getSize());
+  //	Serial.println(" in queue");
 }
 void receiveData()
 {
-	static const char MAGIC = 0x22;
-	static byte data[8][8];
-	static const byte* dataEnd = *data+64;
-	static byte* dataPtr = data[0];
-	static bool isReceivingFrame = false;
+  static const char MAGIC = 0x22;
+  static byte data[8][8];
+  static const byte* dataEnd = *data+64;
+  static byte* dataPtr = data[0];
+  static boolean isReceivingFrame = false;
 
-	if(!isReceivingFrame)
-	{
-		while(Serial.available())
-		{
-			if(Serial.read() == MAGIC)
-			{
-				//Serial.println("magic received");
-				isReceivingFrame = true;
-				break;
-			}
-			
-		}
-	}
+  if(!isReceivingFrame)
+  {
+    //            Serial.println("!isReceivingFrame");
+    while(Serial.available())
+    {
+      if(Serial.read() == MAGIC)
+      {
+//        			Serial.println("magic received");
+        isReceivingFrame = true;
+        break;
+      }
 
-	if(isReceivingFrame)
-	{
-		byte left = dataEnd - dataPtr;
-		byte read = Serial.readBytes((char*)dataPtr, left);
-		if( read  == left ) //frame is ready
-		{
-			//add frame to queue
-			//Serial.println("\t\t\tframe received");
-			queue.enqueue(data);
-			isReceivingFrame = false;
-			//return pointer to start
-			dataPtr = data[0];
-		}
-		else dataPtr += read;
-	}
+    }
+  }
+
+  if(isReceivingFrame && Serial.available() > 0)
+  {
+    //          Serial.println("isReceivingFrame");
+    byte left = dataEnd - dataPtr;
+//    Serial.print("left ");
+//    Serial.println(left);
+    byte read = Serial.readBytes((char*)dataPtr, left);
+//                    Serial.print("received ");
+//    Serial.println(read);
+    if( read  == left ) //frame is ready
+    {
+      //add frame to queue
+//      			Serial.println("\t\t\tframe received");
+      queue.enqueue(data);
+      isReceivingFrame = false;
+      //return pointer to start
+      dataPtr = data[0];
+    }
+    else dataPtr += read;
+  }
 }
 
 void updateCurentFrame()
 {
-	queue.dequeue(data);
+  queue.dequeue(data);
 }
+
