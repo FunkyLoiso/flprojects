cfdca4656ee6e68995557a1e3d4f37849bba99f0
corealugly@gmail.com <corealugly@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2013-02-05 08:21:57 +0000

    поправлено: вычисление угла при зеркалировании рисовании холста перенесено в функцию исправлен конструктор холста не закончено: рисовании фигур рисование холста

--- trunk/lonpos/lonposcanvaswidget.cpp
+++ trunk/lonpos/lonposcanvaswidget.cpp
@@ -8,38 +8,52 @@ LonposCanvasWidget::LonposCanvasWidget(QWidget *parent, int x, int y):
     this->y_height = y;
 }
 
-bool LonposCanvasWidget::setSize(int x, int y)
+void LonposCanvasWidget::setSize(int x, int y)
 {
     this->x_weight = x;
     this->y_height = y;
 }
 
-void LonposCanvasWidget::paintEvent(QPaintEvent *)
+void LonposCanvasWidget::paintFonCircle(int x, int y)
 {
     QPainter paint(this);       //объект рисовальщика
     paint.setRenderHint(QPainter::Antialiasing);
-    paint.save();
-
     //серый фон
-//    paint.restore();
+    paint.save();
     paint.setBrush(Qt::gray);
     paint.setPen(Qt::NoPen);
-    paint.drawRect(0,0,this->width(),this->height());
+    paint.drawRect(0,0,this->width(),this->height());   //квадрат
 
     //полости для фигур
     paint.restore();
     paint.setPen(Qt::NoPen);
     paint.setBrush(Qt::lightGray);
-    int tmp_x = (this->width()/(this->x_weight+1));
-    int tmp_y = (this->height()/(this->y_height+1));
-    for(int yi = 1;yi<=this->y_height;yi++)
+    int tmp_x = (this->width()/(x+1));
+    int tmp_y = (this->height()/(y+1));
+    for(int yi = 1;yi<=y;yi++)
     {
-        for(int xi = 1;xi<=this->x_weight;xi++)
+        for(int xi = 1;xi<=x;xi++)
         {
             paint.drawEllipse(QRectF( tmp_x*xi-tmp_x/2,(tmp_y*yi)-tmp_y/2,tmp_x,tmp_y));
         }
     }
+}
 
+void LonposCanvasWidget::paintFonCircle(M2B canvas, M2B invert_canvas)
+{
+    QPainter paint(this);
+    paint.setRenderHints(QPainter::Antialiasing);
+    paint.save();
+    //серый фон
+    paint.setBrush(Qt::gray);
+    paint.setPen(Qt::NoPen);
+    paint.drawRect(0,0,this->width(),this->height());  // квадрат
 
+    //полости фигур
 
+}
+
+void LonposCanvasWidget::paintEvent(QPaintEvent *)
+{
+    paintFonCircle(x_weight,y_height);
 }
