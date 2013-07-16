b255b811f1a44fe07d2f793009b934a843a940cc
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2013-07-16 09:32:18 +0000

    - now we send accel data as well  - now all bytes are sent separately

--- Anduin/HexPCController/SensorFirmware/SensorFirmware.ino
+++ Anduin/HexPCController/SensorFirmware/SensorFirmware.ino
@@ -1,15 +1,79 @@
+#include <SPI.h>
+
 #define BAUD_RATE 57600
+
+//Assign the Chip Select signal to pin 10.
+int CS=10;
+
+//This is a list of some of the registers available on the ADXL345.
+//see at https://www.sparkfun.com/datasheets/Sensors/Accelerometer/ADXL345.pdf
+byte POWER_CTL = 0x2D;	//Power Control Register
+byte DATA_FORMAT = 0x31;
+byte DATAX0 = 0x32;	//X-Axis Data 0
+byte DATAX1 = 0x33;	//X-Axis Data 1
+byte DATAY0 = 0x34;	//Y-Axis Data 0
+byte DATAY1 = 0x35;	//Y-Axis Data 1
+byte DATAZ0 = 0x36;	//Z-Axis Data 0
+byte DATAZ1 = 0x37;	//Z-Axis Data 1
+
+//This buffer will hold values read from the ADXL345 registers.
+byte values[10];
+//These variables will be used to hold the x,y and z axis accelerometer values.
+int x,y,z;
+//deltas for accel callibration
+int dx = 0, dy = 0, dz = 0;
+
+// 
+//#define MSG_SIZE 6
+//uint8_t msg[MSG_SIZE];
+static const double alpha = 0.3;
+//int filtered[6] = {0};
 
 void setup()
 {
-	Serial.begin(57600);
-	
+	Serial.begin(BAUD_RATE);
+
+	//Initiate an SPI communication instance.
+	SPI.begin();
+	//Configure the SPI connection for the ADXL345.
+	SPI.setDataMode(SPI_MODE3);
+
+	//Set up the Chip Select pin to be an output from the Arduino.
+	pinMode(CS, OUTPUT);
+	//Before communication starts, the Chip Select pin needs to be set high.
+	digitalWrite(CS, HIGH);
+
+	//Put the ADXL345 into +/- 4G range by writing the value 0x01 to the DATA_FORMAT register.
+	//writeRegister(DATA_FORMAT, 0x01);
+	writeRegister(DATA_FORMAT, 0x00);//0x00 - +-2g mode
+	//Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
+	writeRegister(POWER_CTL, 0x08);  //Measurement mode  
+
+	delay(10);
+	for(int i = 0; i < 100; ++i)
+	{
+		readRegister(DATAX0, 6, values);
+
+		//The ADXL345 gives 10-bit acceleration values, but they are stored as bytes (8-bits). To get the full value, two bytes must be combined for each axis.
+		//The X value is stored in values[0] and values[1].
+		x = ((int)values[1]<<8)|(int)values[0];
+		//The Y value is stored in values[2] and values[3].
+		y = ((int)values[3]<<8)|(int)values[2];
+		//The Z value is stored in values[4] and values[5].
+		z = ((int)values[5]<<8)|(int)values[4];
+
+		dx += (0-x);
+		dy += (0-y);
+		dz += (256-z);
+
+		delay(20);
+	}
+	dx /= 100;
+	dy /= 100;
+	dz /= 100;
 }
 
-#define MSG_SIZE 6
-uint8_t msg[MSG_SIZE];
-static const int avgSize = 5;
-int avg[6] = {0};
+
 
 bool started = false;
 
@@ -21,18 +85,69 @@ void loop()
 		started = true;
 	}
 
-	int counter = avgSize;
-	while(--counter >= 0)
-	{
-		for(int i = 0; i < 6; ++i) avg[i] += analogRead(A0+i);
-	}
 	for(int i = 0; i < 6; ++i)
 	{
-		msg[i] = avg[i]/avgSize/4;//we send value/4!
-		avg[i] = 0;
+		//send pressure sensors unfiltered. We send value/4 to fit 10 bit into 8!
+		Serial.write((analogRead(A0+i))/4);
 	}
 
-	Serial.write(msg, MSG_SIZE);
+	readRegister(DATAX0, 6, values);
+	//we filter accel data
+	x = filter( (((int)values[1]<<8)|(int)values[0]) + dx, x, alpha );
+	y = filter( (((int)values[3]<<8)|(int)values[2]) + dy, y, alpha );
+	z = filter( (((int)values[5]<<8)|(int)values[4]) + dz, z, alpha );
+
+	double angleX = atan2(x, z) * RAD_TO_DEG;
+	double angleY = atan2(y, z) * RAD_TO_DEG;
+
+	//we send angel in degrees multiplied by 10 as one byte
+
+	Serial.write(char(angleX*10));
+	Serial.write(char(angleY*10));
+
 
 	if(Serial.read() == 99) started = false;
+}
+
+int filter(int val, int prevFiltered, double alpha)
+{
+	return prevFiltered  + alpha * (val - prevFiltered);
+}
+
+//This function will write a value to a register on the ADXL345.
+//Parameters:
+//  char registerAddress - The register to write a value to
+//  char value - The value to be written to the specified register.
+void writeRegister(byte registerAddress, byte value){
+	//Set Chip Select pin low to signal the beginning of an SPI packet.
+	digitalWrite(CS, LOW);
+	//Transfer the register address over SPI.
+	SPI.transfer(registerAddress);
+	//Transfer the desired register value over SPI.
+	SPI.transfer(value);
+	//Set the Chip Select pin high to signal the end of an SPI packet.
+	digitalWrite(CS, HIGH);
+}
+
+//This function will read a certain number of registers starting from a specified address and store their values in a buffer.
+//Parameters:
+//  char registerAddress - The register addresse to start the read sequence from.
+//  int numBytes - The number of registers that should be read.
+//  char * values - A pointer to a buffer where the results of the operation should be stored.
+void readRegister(byte registerAddress, int numBytes, byte* values){
+	//Since we're performing a read operation, the most significant bit of the register address should be set.
+	char address = 0x80 | registerAddress;
+	//If we're doing a multi-byte read, bit 6 needs to be set as well.
+	if(numBytes > 1)address = address | 0x40;
+
+	//Set the Chip select pin low to start an SPI packet.
+	digitalWrite(CS, LOW);
+	//Transfer the starting register address that needs to be read.
+	SPI.transfer(address);
+	//Continue to read registers until we've read the number specified, storing the results to the input buffer.
+	for(int i=0; i<numBytes; i++){
+		values[i] = SPI.transfer(0x00);
+	}
+	//Set the Chips Select pin high to end the SPI packet.
+	digitalWrite(CS, HIGH);
 }
