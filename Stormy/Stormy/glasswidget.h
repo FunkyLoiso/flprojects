7ec08b2a2c9967abed203fd25e9698aacd82c7bd
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2013-08-05 13:37:30 +0000

    the ui

---
+++ Stormy/Stormy/glasswidget.h
@@ -0,0 +1,16 @@
+#ifndef GLASSWIDGET_H
+#define GLASSWIDGET_H
+
+#include <QWidget>
+
+class GlassWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	GlassWidget(QWidget *parent = 0);
+	~GlassWidget();
+
+};
+
+#endif // GLASSWIDGET_H
