2e2a60f1c836a477435d2a182ce8ccc7c6cf4270
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2013-08-29 23:17:16 +0000

    a simple function to determine particle's move rectangle for a frame

--- Stormy/Stormy/SimplePhysicsEngine.h
+++ Stormy/Stormy/SimplePhysicsEngine.h
@@ -17,6 +17,9 @@ private:
 	void writeProjectedSpeedAndPosition(Particle& p);
 	void doCollisions(Particle& p, int level);//recursively detect and process collisions for p and everything it touches
 	bool findFirstCollision(Particle& p, Collision& out_collision) const;//returns true if collision was detected
+
+	QRectF getBoundingRect(Particle p, QVector2D acceleration, qreal timeLeft) const;
+
 	void processCollision(Collision& c);//update projected speed and position due to collision c
 
 	Glass* m_glass;
