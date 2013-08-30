59f51b800f6e80d45426f7dc64fcb8080e90a3aa
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2013-08-30 17:05:16 +0000

    a faster function rectIntersectsLine

--- Stormy/Stormy/SimplePhysicsEngine.h
+++ Stormy/Stormy/SimplePhysicsEngine.h
@@ -18,9 +18,10 @@ private:
 	void doCollisions(Particle& p, int level);//recursively detect and process collisions for p and everything it touches
 	bool findFirstCollision(Particle& p, Collision& out_collision) const;//returns true if collision was detected
 
-	QRectF getBoundingRect(Particle p, QVector2D acceleration, qreal timeLeft) const;
-
 	void processCollision(Collision& c);//update projected speed and position due to collision c
+	
+	QRectF getBoundingRect(Particle p, QVector2D acceleration, qreal timeLeft) const;
+	bool rectIntersectsLine(const QRectF& rect, QVector2D p1, QVector2D p2) const;
 
 	Glass* m_glass;
 	qreal m_time_s;
