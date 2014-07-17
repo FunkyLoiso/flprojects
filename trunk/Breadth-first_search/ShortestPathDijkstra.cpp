caaf56815e95319829df5ff7fefea64f172af38e
funky.loiso@gmail.com <funky.loiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2014-07-17 07:53:59 +0000

    в алгоритме Дейкстры поиск по списку заменён автоматическим поиском положенного места при вставке/модификации

--- trunk/Breadth-first_search/ShortestPathDijkstra.cpp
+++ trunk/Breadth-first_search/ShortestPathDijkstra.cpp
@@ -1,7 +1,8 @@
 #include "ShortestPathDijkstra.h"
 
-#include <algorithm>
 #include <set>
+#include <unordered_set>
+#include <queue>
 
 ShortestPathDijkstra::ShortestPathDijkstra(const DirectedWeightedGraph& graph, int from, int to)
 {
@@ -9,33 +10,34 @@ ShortestPathDijkstra::ShortestPathDijkstra(const DirectedWeightedGraph& graph, i
     dst[from] = 0.0f;
     std::vector<int> prev(graph.vertCount(), -1);
 
-    std::list<int> verts;
-    std::set<int> visited;
-    verts.push_back(from);
+    std::unordered_set<int> visited;// даёт пространственный выигрыш по сравнению с массивом длины vertCount тольо если ищем один кратчайший путь
 
     auto lessDst = [&dst](int f, int s) {return dst[f] < dst[s];};
+    std::set<int, decltype(lessDst)> verts(lessDst);
+    verts.insert(from);
 
-    while(!verts.empty())
+    while(!verts.empty()) // N * log N
     {
-        auto i = std::min_element(verts.begin(), verts.end(), lessDst);
+        auto i = verts.begin();
         int vert = *i;
         if(vert == to) break;//если целевая вершина здесь, то её расстояние окончательно
         verts.erase(i);
-        visited.insert(vert);
+        visited.insert(vert); // C
 
         auto edgesFromCur = graph.edges(vert);
         for(auto e : edgesFromCur)
         {
-            if(visited.count(e.to) != 0) continue;
-            if(prev[e.to] == -1) verts.push_back(e.to);//добавляем в список на обработку только если встречаем первый раз
+            if(visited.count(e.to) != 0) continue; // C
+            if(prev[e.to] == -1) verts.insert(e.to); // log N. Добавляем в список на обработку только если встречаем первый раз
 
             double thisPathDst = dst[e.from] + e.weight;
             if(thisPathDst < dst[e.to])
             {
                 dst[e.to] = thisPathDst;
+                verts.insert(e.to);// log N. Заменяем, чтобы найти новое место
                 prev[e.to] = e.from;
             }
-            verts.push_back(e.to);
+            verts.insert(e.to); // log N.
         }
     }
 
