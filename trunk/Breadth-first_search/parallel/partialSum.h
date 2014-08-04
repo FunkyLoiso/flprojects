620e59120bfdfc4923e2a8d23e1a70dbc0422049
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2014-08-04 11:36:17 +0000

    более чёткая полследовательность шагов в parallel/partialSum.h

--- trunk/Breadth-first_search/parallel/partialSum.h
+++ trunk/Breadth-first_search/parallel/partialSum.h
@@ -1,6 +1,9 @@
 #ifndef PARTIALSUM_H
 #define PARTIALSUM_H
 
+#include <future>
+#include <vector>
+
 namespace parallel
 {
 
@@ -37,18 +40,45 @@ template <class InIterator, class OutIterator, class BinaryOperation>
    OutIterator partial_sum (InIterator first, InIterator last,
                             OutIterator result, BinaryOperation binary_op)
    {
-       const auto numElements = last-first;
-       __partial_sum_special_step1(first, last, result, binary_op);
+       const size_t numElements = last-first;
+       if(numElements == 0) return result;
+//       const size_t jobsN = std::min(size_t(2), (numElements)/2);//temp
+       //jobSize должен быть чётным
+//       const size_t jobSize = numElements/jobsN;
+//       std::vector<std::future<void>> futures;
 
-       unsigned step = 4;
+//       for(unsigned job = 0; job < jobsN; ++job)
+//       {
+//           InIterator begin = first + job*jobSize;
+//           OutIterator res = result + job*jobSize;
+//           InIterator end;
+//           if(job == jobsN-1) end = last;
+//           else end = begin + jobSize;
+
+////           futures.push_back(std::async( __partial_sum_special_step1<InIterator, OutIterator, BinaryOperation>, begin, end, res, binary_op ));
+//           __partial_sum_special_step1(begin, end, res, binary_op);
+//       }
+//       for(auto& f : futures) f.get();
+//       futures.clear();
+//       __partial_sum_special_step1(first, last, result, binary_op);
+
+       unsigned step = 2;
        for(; step <= numElements; step <<= 1)
        {
-           __partial_sum_step(result, result+numElements, binary_op, step);
+           if(step == 2)
+           {//на первом шаге не только запишем суммы с шагом 2, но так же скопируем все нечётные элементы
+               __partial_sum_special_step1(first, last, result, binary_op);
+           }
+           else
+           {
+               __partial_sum_step(result, result+numElements, binary_op, step);
+           }
        }
 
+       //далее выполняем шаги в обратном направлении, но со сдвигом на пол шага вперёд. Если step + step/2 больше, чем размер коллекции, начинаем с меньшего шага.
+       if(step + (step >> 1) > numElements) step >>= 1;
        for(; step >= 2; step >>= 1)
        {
-           if(step + (step>>1) > numElements) continue;//на эиом этапе мы записываем начитная с step+step/2 элемента. Если такого нет, уменьшаем step.
            __partial_sum_step(result+(step>>1), result+numElements, binary_op, step);
        }
         return result;
