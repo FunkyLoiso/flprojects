aedff585c571b5b32a80e6f38ee1a9f747e033a4
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2014-08-07 13:05:02 +0000

    прямолинейный многопоточный алгоритм partial_sum2, который таки даёт выигрыш

--- trunk/Breadth-first_search/parallel/partialSum.h
+++ trunk/Breadth-first_search/parallel/partialSum.h
@@ -4,6 +4,11 @@
 #include <future>
 #include <vector>
 
+#include <chrono>
+#include <numeric>
+
+using namespace std::chrono;
+
 namespace parallel
 {
 
@@ -40,17 +45,19 @@ void __partial_sum_special_step1 (InIterator first, InIterator last,
 
 template <class InIterator, class OutIterator, class BinaryOperation>
 OutIterator partial_sum (InIterator first, InIterator last,
-                         OutIterator result, BinaryOperation binary_op)
+                         OutIterator result, BinaryOperation binary_op, size_t maxJobs)
 {
     const size_t numElements = last-first;
     if(numElements == 0) return result;
 
-    const size_t maxJobs = 3;// temp
+//    const size_t maxJobs = std::thread::hardware_concurrency();// temp
+//    const size_t maxJobs = 1;
     std::vector<std::future<void>> futures;
 
     size_t step = 2;//текущий шаг суммирования (в каждый step элемент записывается его сумма с отстающим на step/2 назад). Минимальный размер задания равен step.
     for(; step <= numElements; step <<= 1)
     {
+//        high_resolution_clock::time_point t1 = high_resolution_clock::now();
         size_t jobSize = std::max(numElements / maxJobs, step);
         auto remainder = jobSize % step;
         if(remainder != 0)//размер задания должен быть кратен step
@@ -87,7 +94,7 @@ OutIterator partial_sum (InIterator first, InIterator last,
             }
             else
             {
-                OutIterator begin = first + job*jobSize;
+                OutIterator begin = result + job*jobSize;
                 OutIterator end;
                 if(job == jobsN -1) end = result + numElements;
                 else end = begin + jobSize;
@@ -99,6 +106,9 @@ OutIterator partial_sum (InIterator first, InIterator last,
         }
        for(auto& f : futures) f.get();
        futures.clear();
+//       high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+//       std::cout << "step " << step << ": " << (duration_cast<milliseconds>(t2 - t1)).count() << " ms" << std::endl;
     }
 
     //далее выполняем шаги в обратном направлении, но со сдвигом на пол шага вперёд. Если step + step/2 больше, чем размер коллекции, начинаем с меньшего шага.
@@ -138,6 +148,57 @@ OutIterator partial_sum (InIterator first, InIterator last,
         futures.clear();
     }
     return result;
+}
+
+template <class InIterator, class OutIterator, class BinaryOperation>
+OutIterator partial_sum2 (InIterator first, InIterator last,
+                         OutIterator result, BinaryOperation binary_op, size_t jobsCount)
+{
+    const size_t elCount = last - first;
+    if(elCount < 1) return result;
+//    const size_t jobsCount = std::thread::hardware_concurrency();
+    const size_t jobSize = elCount/jobsCount;
+
+    std::vector <std::future<OutIterator>> futures;
+
+    for(unsigned job = 0; job < jobsCount; ++job)
+    {
+        InIterator begin = first + (job*jobSize);
+        OutIterator res = result + (job*jobSize);
+        InIterator end;
+        if(job != jobsCount-1) end = begin + jobSize;
+        else end = last;
+        futures.push_back(std::async( std::partial_sum<InIterator, OutIterator, BinaryOperation>, begin, end, res, binary_op ));
+//        std::partial_sum(begin, end, res, binary_op);
+    }
+
+    std::vector<OutIterator::value_type> sums;
+    for(auto& f : futures)
+    {
+        sums.push_back(f.get()[-1]);
+    }
+    futures.clear();
+
+    std::partial_sum(sums.begin(), sums.end(), sums.begin(), binary_op);
+
+    for(unsigned job = 1; job < jobsCount; ++job)
+    {
+        OutIterator begin = result + (job*jobSize);
+        OutIterator end;
+        if(job != jobsCount-1) end = begin + jobSize;
+        else end = result + elCount;
+
+        const auto valToAdd = sums[job-1];
+        auto adder = [valToAdd](const OutIterator::value_type& val) {return val + valToAdd;};
+        futures.push_back(std::async( std::transform<OutIterator, OutIterator, decltype(adder)>, begin, end, begin, adder ));
+//        std::transform(begin, end, begin, adder);
+    }
+    for(auto& f : futures)
+    {
+        f.get();
+    }
+
+    return result + elCount;
 }
 
 }// namespace parallel
--- trunk/Breadth-first_search/parallel/testParallel.h
+++ trunk/Breadth-first_search/parallel/testParallel.h
@@ -15,24 +15,57 @@ void testParallel()
 //    std::vector<int> v {1, 2, 3, 4, 5, 6, 7, 8, 9};
 //    std::vector<int> result {1, 3, 6, 10, 15, 21, 28, 36, 45};
 
-    for(int i = 0; i < 50; ++i)
+//    for(int i = 0; i < 50; ++i)
+//    {
+//        std::vector<int> v(i);
+//        std::iota(v.begin(), v.end(), 1);
+
+//        std::cout << "\n\nin : ";
+//        printCollection(v);
+//        std::cout << std::endl;
+
+//        std::vector<int> my(v);
+//        parallel::partial_sum(my.begin(), my.end(), my.begin(), std::plus<int>());
+//        std::cout << "\nout: ";
+//        printCollection(my);
+
+//        std::partial_sum(v.begin(), v.end(), v.begin());
+//        assert(my == v);
+//    }
+
+    for(size_t count = 1000000000; count <= 1000000000; count *= 10)
     {
-        std::vector<int> v(i);
+        std::vector<short> v(count);
+//        std::for_each(v.begin(), v.end(), [](short& val) {val = std::rand();});
         std::iota(v.begin(), v.end(), 1);
+        std::vector<short> stdCtrl(v);
 
-        std::cout << "\n\nin : ";
-        printCollection(v);
-        std::cout << std::endl;
+        std::cout << "\npartial sums of " << count << " elements:" << std::endl;
+        using namespace std::chrono;
+        high_resolution_clock::time_point t1, t2;
 
-        std::vector<int> my(v);
-        parallel::partial_sum(my.begin(), my.end(), my.begin(), std::plus<int>());
-        std::cout << "\nout: ";
-        printCollection(my);
+        t1 = high_resolution_clock::now();
+        std::partial_sum(stdCtrl.begin(), stdCtrl.end(), stdCtrl.begin(), std::plus<int>());
+        t2 = high_resolution_clock::now();
+        std::cout << "std : " << (duration_cast<milliseconds>(t2 - t1)).count() << " ms" << std::endl;
 
-        std::partial_sum(v.begin(), v.end(), v.begin());
-        assert(my == v);
+        for(size_t jobsCount = 1; jobsCount <= std::thread::hardware_concurrency()*4; ++jobsCount)
+        {
+            std::vector<short> vCopy(v);
+            t1 = high_resolution_clock::now();
+            parallel::partial_sum2(vCopy.begin(), vCopy.end(), vCopy.begin(), std::plus<int>(), jobsCount);
+            t2 = high_resolution_clock::now();
+            std::cout << "my" << jobsCount << ": " << (duration_cast<milliseconds>(t2 - t1)).count() << " ms" << std::endl;
+
+            if(vCopy != stdCtrl)
+            {
+                std::cout << "my != std!";
+                exit(-1);
+            }
+        }
     }
-
+    std::cout << "\npress return to exit";
+    getchar();
 
 }
 
