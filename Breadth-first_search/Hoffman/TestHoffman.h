223211eb2dfad3eac16788fe2e7a898da02a5139
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2014-08-13 17:43:03 +0000

    архиватор на кодах Хоффмана. Не работает, если в файле все 256 байт представлены равномерно (из-за попытки уместить число 256 в 8 бит). Не работет просто на большом файле. На некоторых файлах работает)

--- Breadth-first_search/Hoffman/TestHoffman.h
+++ Breadth-first_search/Hoffman/TestHoffman.h
@@ -3,6 +3,7 @@
 
 #include "HoffmanTree.h"
 #include "CanonicalHoffmanCoDec.h"
+#include "HoffmanArchiver.h"
 
 #include <fstream>
 #include <iostream>
@@ -20,64 +21,80 @@ std::ostream& operator<<(std::ostream& stream, const HoffmanCode& c)
 
 void testHoffman()
 {
-    std::ifstream file("c:\\test.txt");
-    HoffmanTree t = HoffmanTree::build(file);
-    file.close();
+//    std::ifstream file("c:\\test2.txt", std::ios_base::binary);
+//    HoffmanTree t = HoffmanTree::build(file);
+//    file.close();
 
-    CanonicalHoffmanCoDec codec(t);
+//    CanonicalHoffmanCoDec codec(t);
 
-    for(int i = 0; i <= 255; ++i)
-    {
-        std::cout << char(i) << ": " << codec.encode(i) << std::endl;
-    }
+//    for(int i = 0; i <= 255; ++i)
+//    {
+//        std::cout << char(i) << ": " << codec.encode(i) << std::endl;
+//    }
 
-    for(int i = 0; i <= 0xffff; ++i)
-    {
-        for(int l = 0; l < 255; ++l)
-        {
-            uint8_t val;
-            HoffmanCode c;
-            c.data = i;
-            c.size = l;
+//    for(int i = 0; i <= 0xffff; ++i)
+//    {
+//        for(int l = 0; l < 255; ++l)
+//        {
+//            uint8_t val;
+//            HoffmanCode c;
+//            c.data = i;
+//            c.size = l;
 
-            if(codec.decode(c, val))
-            {
-                std::cout << c << ": " << char(val) << std::endl;
-                assert(c.data == codec.encode(val).data);
-            }
-        }
-    }
+//            if(codec.decode(c, val))
+//            {
+//                std::cout << c << ": " << char(val) << std::endl;
+//                assert(c.data == codec.encode(val).data);
+//            }
+//        }
+//    }
 
-    auto serialization = codec.serialize();
-    std::cout << "Codec serailized size: " << serialization.size() << " bytes" << std::endl;
+//    auto serialization = codec.serialize();
+//    std::cout << "Codec serailized size: " << serialization.size() << " bytes" << std::endl;
 
-    //создадим кодек из сериализации и сравним с оригиналом
-    CanonicalHoffmanCoDec deser = CanonicalHoffmanCoDec::deserialize(serialization);
+//    //создадим кодек из сериализации и сравним с оригиналом
+//    CanonicalHoffmanCoDec deser = CanonicalHoffmanCoDec::deserialize(serialization);
 
-    for(int i = 0; i <= 255; ++i)
-    {
-        assert(codec.encode(i).data == deser.encode(i).data);
-        assert(codec.encode(i).size == deser.encode(i).size);
-    }
+//    for(int i = 0; i <= 255; ++i)
+//    {
+//        assert(codec.encode(i).data == deser.encode(i).data);
+//        assert(codec.encode(i).size == deser.encode(i).size);
+//    }
 
-    for(int i = 0; i <= 0xffff; ++i)
-    {
-        for(int l = 0; l < 255; ++l)
-        {
-            uint8_t val;
-            HoffmanCode c;
-            c.data = i;
-            c.size = l;
+//    for(int i = 0; i <= 0xffff; ++i)
+//    {
+//        for(int l = 0; l < 255; ++l)
+//        {
+//            uint8_t val;
+//            HoffmanCode c;
+//            c.data = i;
+//            c.size = l;
 
-            if(codec.decode(c, val))
-            {
-                uint8_t deserVal;
-                assert(deser.decode(c, deserVal));
-                assert(val == deserVal);
-                assert(c.data == deser.encode(deserVal).data);
-            }
-        }
-    }
+//            if(codec.decode(c, val))
+//            {
+//                uint8_t deserVal;
+//                assert(deser.decode(c, deserVal));
+//                assert(val == deserVal);
+//                assert(c.data == deser.encode(deserVal).data);
+//            }
+//        }
+//    }
+
+
+    //проверка архиватора
+    std::ifstream srcFile("c:\\test4.txt", std::ios_base::binary);
+    std::ofstream compressedFileO("c:\\test.compressed", std::ios_base::binary);
+
+    HoffmanCompress(srcFile, compressedFileO);
+    compressedFileO.close();
+    srcFile.close();
+
+
+    std::ifstream compressedFileI("c:\\test.compressed", std::ios_base::binary);
+    std::ofstream decompressedFile("c:\\test_out.txt", std::ios_base::binary);
+    HoffmanDecompress(compressedFileI, decompressedFile);
+    compressedFileI.close();
+    decompressedFile.close();
 }
 
 #endif // TESTHOFFMAN_H
