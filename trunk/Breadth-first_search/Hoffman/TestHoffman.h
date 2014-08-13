9b3f8c6627b4b01db219ebefa2e21a29ddf8af59
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2014-08-13 13:53:33 +0000

    сериализация/десериализация кодека

--- trunk/Breadth-first_search/Hoffman/TestHoffman.h
+++ trunk/Breadth-first_search/Hoffman/TestHoffman.h
@@ -28,7 +28,6 @@ void testHoffman()
 
     for(int i = 0; i <= 255; ++i)
     {
-//        std::cout << char(i) << ": " << t.code(i) << std::endl;
         std::cout << char(i) << ": " << codec.encode(i) << std::endl;
     }
 
@@ -45,6 +44,37 @@ void testHoffman()
             {
                 std::cout << c << ": " << char(val) << std::endl;
                 assert(c.data == codec.encode(val).data);
+            }
+        }
+    }
+
+    auto serialization = codec.serialize();
+    std::cout << "Codec serailized size: " << serialization.size() << " bytes" << std::endl;
+
+    //создадим кодек из сериализации и сравним с оригиналом
+    CanonicalHoffmanCoDec deser = CanonicalHoffmanCoDec::deserialize(serialization);
+
+    for(int i = 0; i <= 255; ++i)
+    {
+        assert(codec.encode(i).data == deser.encode(i).data);
+        assert(codec.encode(i).size == deser.encode(i).size);
+    }
+
+    for(int i = 0; i <= 0xffff; ++i)
+    {
+        for(int l = 0; l < 255; ++l)
+        {
+            uint8_t val;
+            HoffmanCode c;
+            c.data = i;
+            c.size = l;
+
+            if(codec.decode(c, val))
+            {
+                uint8_t deserVal;
+                assert(deser.decode(c, deserVal));
+                assert(val == deserVal);
+                assert(c.data == deser.encode(deserVal).data);
             }
         }
     }
