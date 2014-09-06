0e0f0f678abff8114ff8ed97283693a0d29d2e99
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2014-09-06 14:53:26 +0000

    хеши неправильно считались для случаев, когда хеш меньше хеша старшего разряда

--- Breadth-first_search/stringMatching/testStringMatching.h
+++ Breadth-first_search/stringMatching/testStringMatching.h
@@ -2,36 +2,95 @@
 #define TESTSTRINGMATCHING_H
 
 #include "RabinKarp.h"
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+
+using namespace std::chrono;
 
 void testStringMatching()
 {
-//    std::string text = "no good dog in doggyland. Nothing";
-//    std::string pattern = "o";
+//    std::string text = "Oh good dog in doggyland. Nothing";
+//    std::string pattern = "good dog";
 //    StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
 //    std::string::iterator i;
 //    while((i = rk.nextMatch()) != text.end())
 //    {
 //        std::cout << i - text.begin() << ", ";
 //    }
-
-    std::string text(1000000, 0);
-    for(auto i = text.begin(); i != text.end(); ++i)
     {
-        *i = rand()%256;
+        std::string text(10000000, 0);
+        for(auto i = text.begin(); i != text.end(); ++i)
+        {
+            *i = rand()%10;
+        }
+
+        std::string pattern(5, 0);
+        for(auto i = pattern.begin(); i != pattern.end(); ++i)
+        {
+            *i = rand()%10;
+        }
+
+        // Рабин-Карп
+        StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
+        std::vector<int> shiftsRK;
+        std::string::iterator i;
+        while((i = rk.nextMatch()) != text.end())
+        {
+                    std::cout << i - text.begin() << ", ";
+            shiftsRK.push_back(i - text.begin());
+        }
+
+        // std
+        std::vector<int> shiftsSTD;
+        i = text.begin();
+        while( (i = std::search(i, text.end(), pattern.begin(), pattern.end())) != text.end() )
+        {
+            shiftsSTD.push_back(i - text.begin());
+            ++i;
+        }
+
+        std::cout << "Rabin-Karp test: " << (shiftsSTD == shiftsRK ? "passed" : "failed") << std::endl;
     }
 
-    std::string pattern(2, 0);
-    for(auto i = pattern.begin(); i != pattern.end(); ++i)
+    // тест производительности
+    for(int patternLength = 1; patternLength <= 4096; patternLength *= 2)
     {
-        *i = rand()%256;
-    }
+        for(int count = 1000000; count <= 100000000; count *= 10)
+        {
+            std::cout << std::endl << "searching " << patternLength << "-char pattern in " << count << "-char string" << std::endl;
+            std::string text(count, 0);
+            for(auto i = text.begin(); i != text.end(); ++i)
+            {
+                *i = rand()%4;
+            }
 
-    StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
-    std::string::iterator i;
-    while((i = rk.nextMatch()) != text.end())
-    {
-        std::cout << i - text.begin() << ", ";
+            std::string pattern(patternLength, 0);
+            for(auto i = pattern.begin(); i != pattern.end(); ++i)
+            {
+                *i = rand()%4;
+            }
+
+            // std
+            std::string::iterator i = text.begin();
+            high_resolution_clock::time_point t1 = high_resolution_clock::now();
+            while( (i = std::search(i, text.end(), pattern.begin(), pattern.end())) != text.end() )
+            {
+                ++i;
+            }
+            high_resolution_clock::time_point t2 = high_resolution_clock::now();
+            std::cout << "std       : " << (duration_cast<milliseconds>(t2 - t1)).count() << " ms" << std::endl;
+
+            // Рабин-Карп
+            StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
+            t1 = high_resolution_clock::now();
+            while((i = rk.nextMatch()) != text.end())
+            {
+            }
+            t2 = high_resolution_clock::now();
+            std::cout << "Rabin-Karp: " << (duration_cast<milliseconds>(t2 - t1)).count() << " ms" << std::endl;
+        }
     }
 }
 
