0748de97152bd342c3ba24cf5f184b501597d2a7
funkyloiso@gmail.com <funkyloiso@gmail.com@0bc2ca54-ee3e-4269-24f0-da2d6dec1318>
2014-09-09 11:04:43 +0000

    Алгоритм поиска подстроки Кнута-Морриса-Пратта

--- Breadth-first_search/stringMatching/testStringMatching.h
+++ Breadth-first_search/stringMatching/testStringMatching.h
@@ -2,6 +2,8 @@
 #define TESTSTRINGMATCHING_H
 
 #include "RabinKarp.h"
+#include "KnuthMorrisPratt.h"
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,11 +13,12 @@ using namespace std::chrono;
 
 void testStringMatching()
 {
-//    std::string text = "Oh good dog in doggyland. Nothing";
-//    std::string pattern = "good dog";
-//    StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
+//    std::string text = "0aa0aa0aa1aa0aa";
+//    std::string pattern = "aa0aa";
+////    StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
+//    StringMatching::KnuthMorrisPratt kmp(text.begin(), text.end(), pattern.begin(), pattern.end());
 //    std::string::iterator i;
-//    while((i = rk.nextMatch()) != text.end())
+//    while((i = kmp.nextMatch()) != text.end())
 //    {
 //        std::cout << i - text.begin() << ", ";
 //    }
@@ -32,26 +35,36 @@ void testStringMatching()
             *i = rand()%10;
         }
 
-        // Рабин-Карп
-        StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
-        std::vector<int> shiftsRK;
-        std::string::iterator i;
-        while((i = rk.nextMatch()) != text.end())
-        {
-                    std::cout << i - text.begin() << ", ";
-            shiftsRK.push_back(i - text.begin());
-        }
-
         // std
         std::vector<int> shiftsSTD;
-        i = text.begin();
+        auto i = text.begin();
         while( (i = std::search(i, text.end(), pattern.begin(), pattern.end())) != text.end() )
         {
             shiftsSTD.push_back(i - text.begin());
             ++i;
         }
 
-        std::cout << "Rabin-Karp test: " << (shiftsSTD == shiftsRK ? "passed" : "failed") << std::endl;
+        // Рабин-Карп
+        StringMatching::RabinKarp rk(text.begin(), text.end(), pattern.begin(), pattern.end());
+        std::vector<int> shiftsRK;
+        while((i = rk.nextMatch()) != text.end())
+        {
+            std::cout << i - text.begin() << ", ";
+            shiftsRK.push_back(i - text.begin());
+        }
+
+        std::cout << std::endl << "Rabin-Karp test: " << (shiftsSTD == shiftsRK ? "passed" : "failed") << std::endl;
+
+        // Кнут-Моррис-Пратт
+        StringMatching::KnuthMorrisPratt kmp(text.begin(), text.end(), pattern.begin(), pattern.end());
+        std::vector<int> shiftsKMP;
+        while((i = kmp.nextMatch()) != text.end())
+        {
+            std::cout << i - text.begin() << ", ";
+            shiftsKMP.push_back(i - text.begin());
+        }
+
+        std::cout << std::endl << "Knuth-Morris-Pratt test: " << (shiftsSTD == shiftsKMP ? "passed" : "failed") << std::endl;
     }
 
     // тест производительности
@@ -90,6 +103,15 @@ void testStringMatching()
             }
             t2 = high_resolution_clock::now();
             std::cout << "Rabin-Karp: " << (duration_cast<milliseconds>(t2 - t1)).count() << " ms" << std::endl;
+
+            // Кнут-Моррис-Пратт
+            StringMatching::KnuthMorrisPratt kmp(text.begin(), text.end(), pattern.begin(), pattern.end());
+            t1 = high_resolution_clock::now();
+            while((i = kmp.nextMatch()) != text.end())
+            {
+            }
+            t2 = high_resolution_clock::now();
+            std::cout << "Knuth-Morris-Pratt: " << (duration_cast<milliseconds>(t2 - t1)).count() << " ms" << std::endl;
         }
     }
 }
